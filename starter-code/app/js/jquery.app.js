8b2b4a22d181567292c91dae19446cbaec20b5c4
CamB17 <camcodes@Camerons-MacBook-Pro.local>
2017-04-19 12:30:37 -0600

    Got new cat to save and post

--- starter-code/app/js/jquery.app.js
+++ starter-code/app/js/jquery.app.js
@@ -1,37 +1,47 @@
-$("new-cat").on("submit", function(event){
+var $catList = $('#cats');
+
+
+$.ajax({
+  method: 'GET',
+  url: 'https://ga-cat-rescue.herokuapp.com/api/cats'
+})
+.done(function(data) {
+        // console.log(data);
+        var obj = JSON.parse(data);
+        // console.log(obj);
+        for (var i = 0; i < obj.length; i++) {
+          var catli = document.createElement('li');
+          catli.innerHTML = obj[i].name + "-" + obj[i].note;
+          $catList.append(catli);        
+        }
+    });
+
+$("#new-cat").submit(function(event) {
+
   event.preventDefault();
-  makeResults();
 
-var catList = $('#cats');
+  var name = $('#cat-name').val();
+  console.log(name);
+  var note = $('#cat-note').val();
+  console.log(note);
 
-var ajax = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
+  var newCat = {
+    name: name,
+    note: note,
+  };
+
+  var catString = JSON.stringify(newCat);
+
+  $.ajax({
+    method: 'POST',
+    url: 'https://ga-cat-rescue.herokuapp.com/api/cats',
+    data: catString
+  });
+  $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
   .done(function(data) {
-    console.log(data)
-    var catsTwo = JSON.parse(data);
-    var catList = document.getElementById('cats');
-    console.log(catsTwo);
-    for(i = 0; i < catsTwo.length; i++) {
-      var catNames = document.createElement('li');
-      catNames.innerHTML = catsTwo[i].name + " - " + catsTwo[i].note;
-      console.log(catNames);
-      catList.append(catNames);   
-    }
-    
-});
+    var newObj = JSON.parse(data);
+    var addCat = newObj.pop();
+    $catList.prepend('<li>' + addCat.name + "-" + addCat.note + '</li>'); 
+  });
 
-    var newList = document.CreateElement('li');
-    var catname = ('cat' + 'name').val();
-    var catnote = ('The GOAT').val();
-    newList.append(catname + "-" + catnote);
-    $('results').append('newList');
-
-     $.ajax ({
-      url:'https://ga-cat-rescue.herokuapp.com/api/cats',
-      data: postData,
-      type: "POST",
-      dataType: 'application/json',
-        success: function(data){
-        console.log(data);
-        catList.append(data);
-    }
 });
